757bfadf34107be724fe6b2bf20ef0d6b2062b63
crizostomo <diogo_crizostomo@hotmail.com>
2022-04-10 17:33:21 -0300

    GenerateOrderHandler class fixed

--- src/main/java/com/dev/designPatterns/order/GenerateOrderHandler.java
+++ src/main/java/com/dev/designPatterns/order/GenerateOrderHandler.java
@@ -1,24 +1,27 @@
 package com.dev.designPatterns.order;
 
+import com.dev.designPatterns.budget.BudgetItem;
 import com.dev.designPatterns.order.action.ActionAfterOrder;
 import com.dev.designPatterns.budget.Budget;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class GenerateOrderHandler {
 
-    private List<ActionAfterOrder> actions;
+    private List<ActionAfterOrder> actionAfterOrders;
 
-    public GenerateOrderHandler(List<ActionAfterOrder> actions) {
-        this.actions = actions;
+    public GenerateOrderHandler(List<ActionAfterOrder> actionAfterOrders) {
+        actionAfterOrders = actionAfterOrders;
     }
 
     public void execute(GenerateOrder data){
-        Budget budget = new Budget(data.getBudgetValue(), data.getItemsQuantity());
+        Budget budget = new Budget();
+        budget.addItem(new BudgetItem(new BigDecimal("200")));
         Order order = new Order(data.getClient(), LocalDateTime.now(), budget);
 
-        actions.forEach(a -> a.executeAction(order));
+        actionAfterOrders.forEach(a -> a.executeAction(order));
 
     }
 }
